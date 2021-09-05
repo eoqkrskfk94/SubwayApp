8ccb81bea5d389d8a563bdb3028278107960dd52
danielkim <37679062+eoqkrskfk94@users.noreply.github.com>
2021-09-05 22:35:57 +0900

    [Add] Jetpack Navigation 셋업

---
+++ app/src/main/java/com/mj/subwayapp/presenter/stations/StationsFragment.kt
@@ -0,0 +1,13 @@
+package com.mj.subwayapp.presenter.stations
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mj.subwayapp.R
+
+class StationsFragment : Fragment() {
+
+
+}
